0ab5a090c6d9ca50ddd00ecfdf57b5834e0a7055
TairFall <tairfall76sfr.fr>
2021-11-17 09:57:50 +0100

    Modification API Paris->Toulouse

--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -13,11 +13,11 @@ class CallApiService
         $this->client = $client;
     }
 
-    public function getParisData(): array
+    public function getToulouseData(): array
     {
         $response = $this->client->request(
             'GET',
-            'https://opendata.paris.fr/api/records/1.0/search/?dataset=dechets-menagers-points-dapport-volontaire-colonnes-a-verre&q=&facet=type_colonne&facet=code_postal&facet=etat&facet=flux'
+            'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&q=&rows=5000&facet=commune&facet=flux'
         );
 
         return $response->toArray();
